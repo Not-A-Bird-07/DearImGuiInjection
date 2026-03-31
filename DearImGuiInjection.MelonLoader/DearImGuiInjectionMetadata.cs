3a1f8f00a5b80404349857d0e005c5b6c438cbcd
notabird <notabird07@gmail.com>
2026-03-31 12:29:28 -0700

    forgot this

--- DearImGuiInjection.MelonLoader/DearImGuiInjectionMetadata.cs
+++ DearImGuiInjection.MelonLoader/DearImGuiInjectionMetadata.cs
@@ -3,7 +3,7 @@
 public sealed class DearImGuiInjectionMetadata
 {
     public const string GUID = Author + "." + Name;
-    public const string Author = "BULLETBOT";
+    public const string Author = "BULLETBOT, Not A Bird";
     public const string Name = "DearImGuiInjection_MelonMono";
     public const string Version = "1.0.3";
 }
